0b483ec7d583213d0578b2236d441e11b9c25df2
Hassan-Zubair10 <hassan.zubair50@gmail.com>
2024-03-06 19:21:31 +0500

    [181] - Employees Earning More Than Their Managers

--- src/employees_earning_more_than_their_managers.sql
+++ src/employees_earning_more_than_their_managers.sql
@@ -2,12 +2,12 @@
 SELECT e.name as Employee
 FROM Employee AS e
 INNER JOIN Employee AS m
-ON e.managerId = m.id AND e.salary > m.salary
+ON e.managerId = m.id AND e.salary > m.salary;
 
 # using self join
 SELECT e.name AS Employee
 FROM Employee e, Employee m
-WHERE e.managerId = m.id AND e.salary > m.salary
+WHERE e.managerId = m.id AND e.salary > m.salary;
 
 # using subquery
 SELECT name as Employee
@@ -15,4 +15,4 @@ FROM Employee AS e
 WHERE salary > (
     SELECT salary FROM Employee
     WHERE id = e.managerId
-)
+);
